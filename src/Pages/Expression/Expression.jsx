c8e876fc096eac0fbeb1041279cdb584197c3079
MP <narbonneperle@gmail.com>
2024-02-28 16:02:27 -0500

    expression-debut

--- src/Pages/Expression/Expression.jsx
+++ src/Pages/Expression/Expression.jsx
@@ -1,11 +1,30 @@
 import ChangePage from "../../Components/ChangePage/ChangePage";
 import "./Expression.scss";
 
-const Expression = ({isDown}) => {
+const Expression = ({ isDown }) => {
   return (
     <div className="expression-container">
-      <ChangePage isDown={isDown}/>
+      <ChangePage isDown={isDown} />
       <h1 className="expression-titre">Expression</h1>
+
+      <div className="text-container">
+        <form action="">
+          <textarea
+            className="expression-textarea"
+            type="textarea"
+            name=""
+            id=""
+            autoFocus
+            wrap="hard"
+            placeholder="Laissez votre créativité s'exprimer ici." />
+        </form>
+        <div className="text-cercle"></div>
+        <div className="text-cercle"></div>
+        <div className="text-cercle"></div>
+        <div className="text-cercle"></div>
+        <div className="text-cercle"></div>
+      </div>
+
     </div>
   );
 };
