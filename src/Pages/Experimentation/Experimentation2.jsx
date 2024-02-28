f8d8d79898985214a3b9ddb12e68d8ee261f67db
MP <narbonneperle@gmail.com>
2024-02-27 23:55:53 -0500

    nouv-anim-experimentation

--- src/Pages/Experimentation/Experimentation2.jsx
+++ src/Pages/Experimentation/Experimentation2.jsx
@@ -7,6 +7,7 @@ import ChangePage from "../../Components/ChangePage/ChangePage";
 const Experimentation2 = ({isDown}) => {
     
     const cibleRef = useRef(null);
+    const composantsRefs = useRef([]);
     useEffect(() => {
         const lettres = Array.from(document.querySelectorAll("h1 .experimentation-lettres"));
         
@@ -42,64 +43,81 @@ const Experimentation2 = ({isDown}) => {
     //             }
     //         });
     // }
-    
-    //     function hoverEnter() {
+
+        const timerExplosion = setTimeout(() => {
+            explosion();
+        }, 2000);
+
+        function explosion() {
+
+            anime({
+                    targets: lettres,
+                    translateX: () => anime.random(-150, 150),
+                    translateY: () => anime.random(-150, 150),
+                    translateZ: () => anime.random(-2000, 750),
+                    rotate: () => anime.random(-250, 250),
+                    easing: 'easeOutElastic(1, 2)',
+                    duration: 750,
+            }); 
+        }
         
-        //         anime({
-            //             targets: lettres,
-            //             translateX: () => anime.random(-150, 150),
-            //             translateY: () => anime.random(-150, 150),
-            //             translateZ: () => anime.random(-2000, 750),
-            //             rotate: () => anime.random(-250, 250),
-            //             easing: 'easeOutElastic(1, 2)',
-            //             duration: 750,
-            //         }); 
-            //     }
-            //     function hoverLeave() {
-                //         anime({
-                    //             targets: lettres,
-                    //             translateX: 0,
-                    //             translateY: 0,
-                    //             translateZ: 0,
-                    //             rotate: 0,
-                    //             easing: 'easeInElastic(1, 2)',
-                    //             duration: 750,
-                    //         }); 
-                    //         console.log(lettres);
-                    //     }
-                    
-                    const handleCible = (e) =>{
-                        console.log(e);
-                    }
-                    
-                    
-                    cibleRef.current.addEventListener("mousedown", handleCible);
-                    
-                    return () => {
-                        cibleRef.current.removeEventListener("mousedown", handleCible);
+        function recomposition() {
+                anime({
+                        targets: lettres,
+                        translateX: 0,
+                        translateY: 0,
+                        translateZ: 0,
+                        rotate: 0,
+                        easing: 'easeInElastic(1, 2)',
+                        duration: 750,
+                    }); 
+                console.log(lettres);
+            }
+            
+            
+        const handleCible = (e) =>{
+            if(e.target === cibleRef.current){
+                recomposition();
+            }else{
+                explosion();
+                console.log(e);
+                const test = document.createElement("div");
+                test.addClasslist("experimentation-tire")
+                test.style.left = e.clientX + "px";
+                test.style.top = e.clientY + "px";
+                e.target.appendChild(test);
+            }
+        }   
+        
+        cibleRef.current.addEventListener("mousedown", handleCible);
+        composantsRefs.current.addEventListener("mousedown", handleCible);
+        
+        return () => {
+            cibleRef.current.removeEventListener("mousedown", handleCible);
+            composantsRefs.current.removeEventListener("mousedown", handleCible);
         };
         
     }, []);
-    
+    // rendu a ajouter la class au div instancier par les cliques
     return (
-        <div className="experimentation-container">
+        <div className="experimentation-container" ref={composantsRefs}>
             <ChangePage isDown={isDown}/>
-            <h1 className="experimentation-titre" >
-                <p className="experimentation-lettres">E</p>
-                <p className="experimentation-lettres">x</p>
-                <p className="experimentation-lettres">p</p>
-                <p className="experimentation-lettres">é</p>
-                <p className="experimentation-lettres">r</p>
-                <p className="experimentation-lettres">i</p>
-                <p className="experimentation-lettres">m</p>
-                <p className="experimentation-lettres">e</p>
-                <p className="experimentation-lettres">n</p>
-                <p className="experimentation-lettres">t</p>
-                <p className="experimentation-lettres">a</p>
-                <p className="experimentation-lettres">t</p>
-                <p className="experimentation-lettres">i</p>
-                <p className="experimentation-lettres">o</p>
-                <p className="experimentation-lettres">n</p>
+            <h1 className="experimentation-titre" ref={composantsRefs}>
+                <p className="experimentation-lettres" ref={composantsRefs}>E</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>x</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>p</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>é</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>r</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>i</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>m</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>e</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>n</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>t</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>a</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>t</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>i</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>o</p>
+                <p className="experimentation-lettres" ref={composantsRefs}>n</p>
             </h1>
             <div className="experimentation-cible" ref={cibleRef}></div>
         </div>
