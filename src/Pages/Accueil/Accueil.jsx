1aec503beae70466b89a7f16e3d0e4b7ba8bd4ac
MP <narbonneperle@gmail.com>
2024-03-01 09:22:11 -0500

    gfdf

--- src/Pages/Accueil/Accueil.jsx
+++ src/Pages/Accueil/Accueil.jsx
@@ -18,7 +18,7 @@ const Accueil = ({isDown}) => {
         <BgAccueil/>
         <div className="accueil-container">
             <h1 className="accueil-titre">Kreativ</h1>
-            <h2 className="accueil-slogan">Exprérience Intéractive</h2>
+            <h2 className="accueil-slogan">Exprérience interactive</h2>
         </div>
         <ChangePage isDown={isDown}/>
         <Instruction texte={"Maintenez la barre d'espacement enfoncée"} delais={6000}/>
